0c9f1a04a15236315227bcd4870ca91ccc9bed47
shanirub <shani.rubovitch@gmail.com>
2019-03-06 15:44:37 +0100

    now node and running

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 const binaryArrayToNumber = arr => {
-    // your code
-  };
+    let dec = 0
+  
+    for(let i = 0; i < arr.length; i++) {
+          let lastDigit = arr.pop()
+          dec += lastDigit * (2 ** i)
+    }
+      
+    return dec
+};
+
+console.log(binaryArrayToNumber([0,0,0,1]))
